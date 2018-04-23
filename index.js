721047467fc46ee3db79bbe9c5258872f726db9c
bietkul <kuldeep@sahusoft.com>
2018-04-23 11:39:38 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import PaymentOptions from './paymentOptions'
+
+export {
+    PaymentOptions
+}
---
+++ paymentOptions/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+class PaymentOptions extends Component {
+    render() {
+        return(
+            <View>
+                <Text>
+                    This is Payment sdk
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default PaymentMethods
